7e1f186f5394968e4a47d5f03a2a8fa181f99ed9
Aidan <aidankmcalister@gmail.com>
2024-10-03 12:55:04 -0500

    date and time

--- app/components/FileList.tsx
+++ app/components/FileList.tsx
@@ -85,8 +85,8 @@ export const FileList = ({
                     <CardTitle>{truncate(file.name, 25)}</CardTitle>
                     <CardDescription className="font-light">
                       Size: {formatFileSize(file.size)}, Uploaded:{" "}
-                      {new Date(file.lastModified).toLocaleDateString()}, Type:{" "}
-                      .{file.type}
+                      {new Date(file.lastModified).toLocaleString()}, Type: .
+                      {file.type}
                     </CardDescription>
                   </CardHeader>
                   <CardContent className="flex items-center justify-center">
